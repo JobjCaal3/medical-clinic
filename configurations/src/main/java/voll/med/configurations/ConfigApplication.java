0dddf231ee2e6cde13d388497b455833fc6d39ff
Job Juarez Caal <jobjcaal54@gmail.com>
2024-10-18 23:12:23 -0600

    minor changes

--- configurations/src/main/java/voll/med/configurations/ConfigApplication.java
+++ configurations/src/main/java/voll/med/configurations/ConfigApplication.java
@@ -1,9 +1,11 @@
-package voll.med.config;
+package voll.med.configurations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigApplication {
 
 	public static void main(String[] args) {
