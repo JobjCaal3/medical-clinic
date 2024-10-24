4c23c53c4cc49cd2fa9773fa233ffc39cf43c8b9
Job Juarez Caal <jobjcaal54@gmail.com>
2024-10-24 14:54:24 -0600

    patient and doctor settings

--- doctors/src/main/java/voll/med/doctors/DoctorsApplication.java
+++ doctors/src/main/java/voll/med/doctors/DoctorsApplication.java
@@ -2,8 +2,10 @@ package voll.med.doctors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DoctorsApplication {
 
 	public static void main(String[] args) {
